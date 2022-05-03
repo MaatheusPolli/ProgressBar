5a20d018e8a1850ab377feea9d1cdf3e9065c929
Wesley Gomes <gomes.wesley@outlook.com.br>
2022-05-03 20:31:14 -0300

    Nova progressbar

---
+++ teste3/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('[data-js="change-percentual"]'); 
+const inputValue = document.querySelector('[data-js="input-value"]');
+const percentBar = document.querySelector('[data-js="africa"]');
+const spanPercent = document.querySelector('[data-js="africa"] span');
+
+button.addEventListener('click', () => {
+    percentBar.style.width = `${inputValue.value}%`;
+    spanPercent.innerHTML = `${inputValue.value}%`;
+});
