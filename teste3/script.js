e0085df68babab27db3d6489bbc2689ea05bbf14
Wesley Gomes <gomes.wesley@outlook.com.br>
2022-05-03 20:50:30 -0300

    Progress Bar correta

--- teste3/script.js
+++ teste3/script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('[data-js="change-percentual"]'); 
 const inputValue = document.querySelector('[data-js="input-value"]');
 const percentBar = document.querySelector('[data-js="africa"]');
-const spanPercent = document.querySelector('[data-js="africa"] span');
+const spanPercent = document.querySelector('[data-js="africa"] .percent');
 
 button.addEventListener('click', () => {
     percentBar.style.width = `${inputValue.value}%`;
